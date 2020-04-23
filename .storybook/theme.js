7e5c222864047c37c8798c362b8119d40ec07cd7
Henrique Augusto <hick_97@hotmail.com>
2020-04-23 13:27:16 -0300

    fix: add a .storybook folder

---
+++ .storybook/theme.js
@@ -0,0 +1,20 @@
+import { create } from "@storybook/theming";
+
+export default create({
+  base: "dark",
+
+  colorSecondary: "#FF8700",
+
+  appBg: "#0b0a0d",
+  appContentBg: "#16161A",
+  appBorderColor: "#FF8700",
+
+  textColor: "#fff",
+
+  // Toolbar default and active colors
+  barTextColor: "#E6E6E6",
+  barSelectedColor: "#ffffff",
+  barBg: "#1b1a1f",
+
+  brandTitle: "UI Kit - Test",
+});
